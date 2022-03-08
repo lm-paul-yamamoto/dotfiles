65980aaae7ce2223487fe137395f4f62514c021e
lm-paul-yamamoto <yamamoto.takashi@lmi-partners.page>
2022-03-08 12:08:11 +0900

    Frist commit

---
+++ .zshrc
@@ -0,0 +1,4 @@
+source "${HOME}/antigen.zsh"
+
+# include .antigenrc
+[[ -f $HOME/.antigenrc ]] && source $HOME/.antigenrc
---
+++ install.sh
@@ -0,0 +1,4 @@
+# !/bin/sh
+curl -L git.io/antigen > antigen.zsh
+ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.antigenrc ~/.antigenrc
